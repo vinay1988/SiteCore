95d1bbcc9e4d565072f0a36c7d0e1107a70ef4d2
vinay kumar mishra <vinay.mishra@live.com>
2017-12-29 18:05:46 +0530

    Work on moule 8

---
+++ events.tac.local/Controllers/OverviewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using events.tac.local.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Links;
+using Sitecore.Web.UI.WebControls;
+
+namespace events.tac.local.Controllers
+{
+    public class OverviewController : Controller
+    {
+        // GET: Overview
+        public ActionResult Index()
+        {
+            var model = new OverviewList()
+            {
+                ReadMore = Sitecore.Globalization.Translate.Text("Read More")
+            };
+
+            model.AddRange(RenderingContext.Current.ContextItem.GetChildren()
+                .Select(i => new OverviewItem()
+                {
+                    Url=LinkManager.GetItemUrl(i),
+                    Title=new HtmlString(FieldRenderer.Render(i,"contentheading")),
+                    Image = new HtmlString(FieldRenderer.Render(i, "DecorationBanner","mw=500&mh=333")),
+                }
+                ));
+
+            return View(model);
+        }
+    }
+}
---
+++ events.tac.local/Models/OverviewItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class OverviewItem
+    {
+        public OverviewItem()
+        {
+
+        }
+        public HtmlString Title { get; set; }
+        public HtmlString Image { get; set; }
+        public string Url { get; set; }
+    }
+}
