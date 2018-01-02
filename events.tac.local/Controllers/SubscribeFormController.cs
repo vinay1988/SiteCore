7ec18fdf3538395ae687271aadb2b0e17516c781
vinay kumar mishra <vinay.mishra@live.com>
2018-01-02 12:22:07 +0530

    submit form in sitecore

---
+++ events.tac.local/Controllers/SubscribeFormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class SubscribeFormController : Controller
+    {
+        // GET: SubscribeForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string email)
+        {
+            return View("Confirmation");
+        }
+    }
+}
